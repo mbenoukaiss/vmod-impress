fdddbc934675d2412168075691dd87c70ede1dc0
mbenoukaiss <mbenoukaiss@gmail.com>
2026-03-12 20:06:45 +0100

    Bump dependencies

--- src/images/jpeg.rs
+++ src/images/jpeg.rs
@@ -1,5 +1,5 @@
 use image::{DynamicImage, EncodableLayout};
-use turbojpeg::{OwnedBuf, Subsamp};
+use turbojpeg::{compress, Image, OwnedBuf, PixelFormat, Subsamp};
 use crate::error::Error;
 use crate::images::OptimizedImage;
 
@@ -20,10 +20,21 @@ impl From<OwnedBuf> for Jpeg {
 }
 
 pub fn to_jpeg(image: &DynamicImage, quality: f32, _prefer_quality: bool) -> Result<Jpeg, Error> {
-    match image {
-        DynamicImage::ImageRgb8(image) => Ok(turbojpeg::compress_image(image, quality as i32, Subsamp::None)?.into()),
-        DynamicImage::ImageRgba8(image) => Ok(turbojpeg::compress_image(image, quality as i32, Subsamp::None)?.into()),
-        _ => Error::err("Unsupported image format"),
-    }
+    //replicates turbojpeg::compress_image but without the `image` cargo feature,
+    //which would force a specific image-crate version onto our dep tree
+    let (pixels, format, width, height) = match image {
+        DynamicImage::ImageRgb8(img) => (img.as_raw().as_slice(), PixelFormat::RGB, img.width(), img.height()),
+        DynamicImage::ImageRgba8(img) => (img.as_raw().as_slice(), PixelFormat::RGBA, img.width(), img.height()),
+        _ => return Error::err("Unsupported image format"),
+    };
 
+    let tj_image = Image {
+        pixels,
+        width: width as usize,
+        pitch: format.size() * width as usize,
+        height: height as usize,
+        format,
+    };
+
+    Ok(compress(tj_image, quality as i32, Subsamp::None)?.into())
 }
